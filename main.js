4a80f6b18a8c7444a27280e318cdac3328179b8a
Jeffrey Na <jeffreyna@Jeffreys-MacBook-Air.local>
2023-04-19 20:39:07 -0400

    completed functionality

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 
-cat.addEventListener('mousemove', (event) => {
-  
+
+document.addEventListener('mousemove', (event) => {
+    // we need to get x and y coordinates of mouse cursor
+    console.log(`Mouse X: ${event.clientX}, Mouse Y: 
+    ${event.clientY}`);
+    // place the pug 50 pixels from the cursor
+    // console.log(document.querySelector("#pug".style.top));
+    // console.log(document.querySelector("#pug").style.top);
+    // const x = event.clientX;
+    // const y = event.clientY;
+    console.log('x: ' + `${event.clientX - 10}`);
+    console.log('y: ' + `${event.clientY - 10}`);
+    document.querySelector("#pug").style.top = `${event.clientY - 10}px`;
+    document.querySelector("#pug").style.left = `${event.clientX - 10}px`;
 });
+
